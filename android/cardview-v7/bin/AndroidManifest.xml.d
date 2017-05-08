98cf92fbd0b6a883c3f8cd3729bbfe53b1c4d55a
Bijosh T J <bijoshtj@gmail.com>
2017-05-08 10:39:04 +0530

    removed bin and gen directories android

--- android/cardview-v7/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/bijosh/Work/devkit2_projects/sudoku-quest-mobile/modules/facebook/android/cardview-v7/bin/AndroidManifest.xml : \
-/home/bijosh/Work/devkit2_projects/sudoku-quest-mobile/modules/facebook/android/cardview-v7/AndroidManifest.xml \
